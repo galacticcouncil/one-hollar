699e984a0e722344cf11eca348ad4a3947876e58
martinfridrich <mrtnfridrich@gmail.com>
2025-09-03 16:11:30 +0200

    test bin search - flips pools in some cases

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ const api = await ApiPromise.create({
 const sdk = await createSdkContext(api);
 const HOLLAR = "222"
 
-const BIN_SEARCH_ITER = 10;
+const BIN_SEARCH_ITER = 20;
+const ZERO = new Big("0")
 const TWO = new Big("2.0")
-const PRECISSION = new Big("0.001")
+const PRECISSION = new Big("0.0001")
 
 class AssetRegistry {
 	#api
@@ -59,30 +60,34 @@ async function findSellTrade(assets, minAmt, maxAmount, price) {
 	console.log(`--> findSellTrade: assets=[${assets}], ${minAmt}, ${maxAmount}, ${price}`)
 	const router = sdk.api.router
 
-	let trade = await router.getBestSell(...assets, maxAmount)
-	let execPrice = (new Big(trade.amountOut)).div(new Big(trade.amountIn))
-	if (execPrice.gte(price)) {
-		return [trade, execPrice]
-	}
-
-	trade = null
-	execPrice = null
+	let trade
+	let execPrice = new Big(0)
 	let amtHigh = new Big(maxAmount)
 	let amtLow = new Big(minAmt)
 	let amt = new Big(minAmt)
 	for(let i = 0; i< BIN_SEARCH_ITER; i++) {
+		console.log(`${i}: amt=${amt.toFixed(5)}, amtLow=${amtLow.toFixed(5)}, amtHigh=${amtHigh.toFixed(5)}`)
+		if (amtLow.eq(amtHigh)) {
+			break
+		}
 		let t = await router.getBestSell(...assets, amt)
-		let tradePrice = (new Big(t.amountOut)).div(new Big(t.amountIn))
+		let tPrice = (new Big(t.amountOut)).div(new Big(t.amountIn))
 
-		if (tradePrice.gte(price)) {
+		console.log(`${t.amountIn}, ${t.amountOut}`)
+		console.log(`in=${t.amountOut}, out=${t.amountIn}`)
+		console.log(`inBig=${(new Big(t.amountIn)).toFixed(5)}, outBig=${(new Big(t.amountOut)).toFixed(5)}`)
+		console.log(t.toHuman())
+		console.log(`${i}: trade_price=${tPrice.toFixed(5)}, target_price=${price}`)
+		if (tPrice.minus(price).abs().lte(PRECISSION)) {
+			//NOTE: close enough
+			break
+		}
+
+		if (tPrice.gt(price) && !trade.amountOut.eq(ZERO)) {
 			amtLow = amt
 			trade = t
-			execPrice = tradePrice
+			execPrice = tprice
 
-			//NOTE: close enough
-			if (tradePrice.sub(price).lte(PRECISSION)) {
-				break
-			}
 		} else {
 			amtHigh = amt
 		}
@@ -91,10 +96,6 @@ async function findSellTrade(assets, minAmt, maxAmount, price) {
 		amt = amtLow.add((amtHigh.sub(amtLow)).div(TWO))
 	}
 
-	console.log("===")
-	console.log(`exec_price=${execPrice.toFixed(5)}, target_price=${price.toFixed(5)}`)
-	console.log(trade.toHuman())
-	console.log("===")
 	return [trade, execPrice]
 }
 
@@ -102,52 +103,64 @@ async function findBuyTrade(assets, minAmt, maxAmount, price) {
 	console.log(`---> findBuyTrade: assets=[${assets}], ${minAmt}, ${maxAmount}, ${price}`)
 	const router = sdk.api.router
 
-	let trade = await router.getBestBuy(...assets, maxAmount)
-	let execPrice = (new Big(trade.amountIn)).div(new Big(trade.amountOut))
-	if (execPrice.lte(price)) {
-		return [trade, execPrice]
-	}
-
-	trade = null
-	execPrice = null
+	let trade
+	let execPrice = new Big(0)
 	let amtHigh = new Big(maxAmount)
 	let amtLow = new Big(minAmt)
 	let amt = new Big(minAmt)
 	for(let i = 0; i< BIN_SEARCH_ITER; i++) {
 		console.log(`amt=${amt.toFixed(5)}, amtLow=${amtLow.toFixed(5)}, amtHigh=${amtHigh.toFixed(5)}`)
-		let t = await router.getBestBuy(...assets, amt)
-		let tradePrice = (new Big(t.amountIn)).div(new Big(t.amountOut))
+		if (amtLow.eq(amtHigh)) {
+			break
+		}
 
-		if (tradePrice.gte(price)) {
+		let t = await router.getBestBuy(...assets, amt)
+		let tPrice = (new Big(t.amountIn)).div(new Big(t.amountOut))
+
+		console.log(`${t.amountIn}, ${t.amountOut}`)
+		console.log(`---> trade_price=${tPrice.toFixed(6)}, price=${price.toFixed(5)}`)
+		if (tPrice.minus(price).abs().lte(PRECISSION)) {
+			//NOTE: close enough
+			break
+		}
+
+		if (tPrice.gt(price) && !t.amountIn.eq(ZERO)) {
 			amtHigh = amt
 		} else {
 			amtLow = amt
 			trade = t
-			execPrice = tradePrice
-
-			//NOTE: close enough
-			if (price.sub(tradePrice).lte(PRECISSION)) {
-				break
-			}
+			execPrice = tPrice
 		}
 
 		//NOTE: make sure this doesn't have to be int
 		amt = amtLow.add((amtHigh.sub(amtLow)).div(TWO))
 	}
 
-	console.log("===")
-	console.log(`exec_price=${execPrice.toFixed(5)}, target_price=${price.toFixed(5)}`)
-	console.log(trade.toHuman())
-	console.log("===")
 	return [trade, execPrice]
 }
 
 async function findTrade(opp, ag) {
 	console.log(opp)
 	if (opp.trade == "sell") {
-		await findSellTrade(opp.assets, opp.minAmount, opp.maxAmount, opp.targetPriceUSD)
+		const [t, execPrice] =await findSellTrade(opp.assets, opp.minAmount, opp.maxAmount, opp.targetPriceUSD)
+		if (!t) {
+			console.log("no sell trade found")
+		} else {
+			console.log("-- found sell trade --") 
+			console.log(`${t.toHuman()}`)
+			console.log(`exec_price=${execPrice.toFixed(5)}, target_price=${opp.targetPriceUSD.toFixed(5)}`)
+			console.log("-- end --") 
+		}
 	} else {
-		await findBuyTrade(opp.assets, opp.minAmount, opp.maxAmount, opp.targetPriceUSD)
+		const [t, execPrice] = await findBuyTrade(opp.assets, opp.minAmount, opp.maxAmount, opp.targetPriceUSD)
+		if (!t) {
+			console.log("no buy trade found")
+		} else {
+			console.log("-- found buy trade --") 
+			console.log(`${t.toHuman()}`)
+			console.log(`exec_price=${execPrice.toFixed(5)}, target_price=${opp.targetPriceUSD.toFixed(5)}`)
+			console.log("-- end --") 
+		}
 	}
 
 	// const p = [...opp.assets, opp.amount]
@@ -178,7 +191,7 @@ async function findTrade(opp, ag) {
 	await reg.update()	
 
 	const evm = new EvmClient(api)
-	const s = new Strategy(sdk, evm, cfg.assets, HOLLAR)
+	const s = new Strategy(sdk, evm, cfg.assets, HOLLAR, reg)
 	const agAssets = Object.values(cfg.assets).map(v => v.assetId);
 	agAssets.push(HOLLAR);
 	const ag = new Agent(api, secretPath, secretPwd, agAssets, reg)
@@ -186,22 +199,28 @@ async function findTrade(opp, ag) {
 	api.derive.chain.subscribeNewHeads(async (header) => {
 		await ag.updateBalances()
 		const opps = await s.findOpportunities()
+		
 		console.log(`INFO: START processing block=${header.number}`)
 
-		const trades = []
-		for (const opp of opps ) {
-			console.log(`INFO: opportunity=${opp.trade}, assets=[${opp.assets}], price=${opp.price}, amount=${opp.amount}`)
-			const trade = await findTrade(opp, ag)
-			if (trade) {
-				trades.push(trade);
-			}
+
+		for (const opp of opps) {
+			console.log(opp.toHuman())
 		}
+
+		const trades = []
+		// for (const opp of opps ) {
+		// 	console.log(`INFO: opportunity=${opp.trade}, assets=[${opp.assets}], price=${opp.price}, amount=${opp.amount}`)
+		// 	const trade = await findTrade(opp, ag)
+		// 	if (trade) {
+		// 		trades.push(trade);
+		// 	}
+		// }
 
 		if (header.number % cfg.cooldown == 0) {
 			if (trades.length == 0) {
-				console.log(`INFO: no trades found in block=${header.number}`)
+				// console.log(`INFO: no trades found in block=${header.number}`)
 			} else {
-				await executeTrades(ag, trades)
+				// await executeTrades(ag, trades)
 				console.log(`INFO: trades submitted`)
 			}
 		} else {
@@ -245,4 +264,10 @@ function loadConfig(path) {
 	}
 
 	return cfg
+}
+
+//TODO: tmp, refactor out
+function toDecimal(num, decimals) {
+  const divisor = Big(10).pow(decimals);
+  return num.div(divisor);
 }
--- strategy.js
+++ strategy.js
@@ -4,20 +4,27 @@ import { ApiPromise } from '@polkadot/api';
 import assert from 'node:assert';
 import Big from 'big.js';
 
-const ZERO = new Big(0)
-const ONE = new Big(1.0)
+const BIN_SEARCH_ITER = 20;
+const ZERO = new Big("0")
+const ONE = new Big("1.0")
+const TWO = new Big("2.0")
+const PRECISSION = new Big("0.0001")
 
 export class Strategy {
 	#sdk
 	#config 
 	#hollar
 	#mmOracle
+	#registry
+	#router
 
-	constructor(sdk, evm, config, hollar) {
+	constructor(sdk, evm, config, hollar, reg) {
 		this.#sdk = sdk	
 		this.#config = config
 		this.#hollar = hollar
 		this.#mmOracle = new MmOracleClient(evm)
+		this.#registry = reg
+		this.#router = sdk.api.router
 	}
 
 	async findOpportunities() {
@@ -39,12 +46,21 @@ export class Strategy {
 			const targetPrice = ONE.div(usdPrice)
 			const sellAt = ONE.div(usdPrice.sub(usdPrice.mul(cfg.sell.priceDiff)))
 			const buyAt = ONE.div(usdPrice.add(usdPrice.mul(cfg.buy.priceDiff)))
-			console.log(`INFO: id: ${p.id}, price=${p.price.toFixed(3)}[${p.id}/H], target_usd_price=${usdPrice.toFixed(3)}[$/${p.id}], target_price=${targetPrice.toFixed(3)}[${p.id}/H], buy_at=${buyAt.toFixed(3)}[${p.id}/H], sell_at=${sellAt.toFixed(3)}[${p.id}/H]`)
+			console.log(`INFO: id: ${p.id}, price=${p.price.toFixed(5)}[${p.id}/H], target_usd_price=${usdPrice.toFixed(5)}[$/${p.id}], target_price=${targetPrice.toFixed(5)}[${p.id}/H], buy_at_limit=${buyAt.toFixed(5)}[${p.id}/H], sell_at_limit=${sellAt.toFixed(5)}[${p.id}/H]`)
 
+			let trade 
 			if (p.price.gte(sellAt)) {
-				opps.push(this.#newHollarOpp("sell", cfg.assetId, cfg.sell.minAmount, cfg.sell.maxAmount, p.price.toString(), sellAt));
+				let [t, execPrice] = await this.findSellTrade([this.#hollar, cfg.assetId], cfg.sell.minAmount, cfg.sell.maxAmount, sellAt)
+				trade = t
 			} else if (p.price.lte(buyAt)) {
-				opps.push(this.#newHollarOpp("buy", cfg.assetId, cfg.buy.minAmount, cfg.buy.maxAmount, p.price.toString(), buyAt));
+				let [t, execPrice] = await this.findBuyTrade([cfg.assetId, this.#hollar], cfg.sell.minAmount, cfg.sell.maxAmount, buyAt)
+				trade = t 
+			}
+
+			if (trade) {
+				opps.push(trade);
+				console.log(trade.toHuman())
+				process.exit(1)
 			}
 		}
 
@@ -54,7 +70,7 @@ export class Strategy {
 	async getHollarPrices(pairs) {
 		const rawPrices = []
 		pairs.forEach(pair => {
-			rawPrices.push(this.#sdk.api.router.getBestSpotPrice(...pair.assets));
+			rawPrices.push(this.#router.getBestSpotPrice(...pair.assets));
 		});	
 
 		const results = await Promise.allSettled(rawPrices) 
@@ -95,10 +111,90 @@ export class Strategy {
 			return { trade: "buy",  assets: [assetId, this.#hollar], minAmount: minAmount, maxAmount: maxAmount, price: price, targetPriceUSD: targetPrice };
 		}
 	}
+
+
+
+	async findSellTrade(assets, minAmt, maxAmount, price) {
+		console.log(`--> findSellTrade: assets=[${assets}], ${minAmt}, ${maxAmount}, ${price}`)
+		const limitPrice = new Big(price)
+		let trade
+		let execPrice = new Big(0)
+		let amtHigh = new Big(maxAmount)
+		let amtLow = new Big(minAmt)
+		let amt = new Big(0)
+		for (let i = 0; i< BIN_SEARCH_ITER; i++) {
+			amt = amtLow.add((amtHigh.sub(amtLow)).div(TWO))
+			console.log(`${i}: amt=${amt.toFixed(5)}, amtLow=${amtLow.toFixed(5)}, amtHigh=${amtHigh.toFixed(5)}`)
+			if (amtLow.eq(amtHigh)) {
+				break
+			}
+
+			let t = await this.#router.getBestSell(...assets, amt)
+			const amtIn = new Big(toDecimal(t.amountIn, this.#registry.decimals(assets[0])))
+			const amtOut = new Big(toDecimal(t.amountOut, this.#registry.decimals(assets[1])))
+			const tPrice = amtOut.div(amtIn) //[a]/[H]
+
+			console.log(`amt_in=${amtIn}, amtOut=${amtOut}, price=${tPrice.toFixed(5)}, limit_price=${limitPrice.toFixed(5)}`)
+			if (tPrice.gte(limitPrice) && amountOut.gt(ONE)) {
+				amtLow = amt
+				trade = t
+				execPrice = tPrice
+
+				if (tPrice.minus(limitPrice).abs().lte(PRECISSION)) {
+					//NOTE: close enough
+					break
+				}
+			} else {
+				amtHigh = amt
+			}
+		}
+
+		console.log("-- find sell end --")
+		return [trade, execPrice]
+	}
+
+	async findBuyTrade(assets, minAmt, maxAmount, price) {
+		console.log(`---> findBuyTrade: assets=[${assets}], ${minAmt}, ${maxAmount}, ${price}`)
+		const limitPrice = new Big(price)
+		let trade
+		let execPrice = new Big(0)
+		let amtHigh = new Big(maxAmount)
+		let amtLow = new Big(minAmt)
+		let amt = new Big(0)
+		for(let i = 0; i< BIN_SEARCH_ITER; i++) {
+			amt = amtLow.add((amtHigh.sub(amtLow)).div(TWO))
+			console.log(`amt=${amt.toFixed(5)}, amtLow=${amtLow.toFixed(5)}, amtHigh=${amtHigh.toFixed(5)}`)
+			if (amtLow.eq(amtHigh)) {
+				break
+			}
+
+			let t = await this.#router.getBestBuy(...assets, amt)
+			const amtIn = new Big(toDecimal(t.amountIn, this.#registry.decimals(assets[0])))
+			const amtOut = new Big(toDecimal(t.amountOut, this.#registry.decimals(assets[1])))
+			console.log(`---> amtIn=${t.amountIn}, amtOut=${amtOut}`)
+			let tPrice = amtIn.div(amtOut) //[a]/[H]
+
+			console.log(`---> trade_price=${tPrice.toFixed(6)}, price=${limitPrice.toFixed(5)}`)
+			if (tPrice.lte(limitPrice) && amtIn.gt(ONE)) {
+				amtLow = amt
+				trade = t
+				execPrice = tPrice
+
+				if (tPrice.minus(limitPrice).abs().lte(PRECISSION)) {
+					//NOTE: close enough
+					break
+				}
+			} else {
+				amtHigh = amt
+			}
+		}
+
+		return [trade, execPrice]
+	}
 }
 
 function toDecimal(num, decimals) {
-  const divisor = Big(10).pow(decimals);
-  return num.div(divisor);
+	const divisor = Big(10).pow(decimals);
+	return num.div(divisor);
 }
 
